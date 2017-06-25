fbc3d02d7198f3e282dfbceef99cf021c6b7ef09
lettas <lettas0726@gmail.com>
2017-06-25 08:40:10 +0600

    ButtonのtouchUpInsideイベントを接続

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -19,5 +19,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
