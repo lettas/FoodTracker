1a835a3b620834c4068d43f848f799bb802f507d
lettas <lettas0726@gmail.com>
2017-06-27 07:32:55 +0600

    イニシャライザを追加

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,4 +10,13 @@ import UIKit
 
 class RatingControl: UIStackView {
 
+    // MARK: Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
