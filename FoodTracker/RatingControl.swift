dcd428c41c1047fd8d60d38135d0eb71b07edb63
lettas <lettas0726@gmail.com>
2017-06-27 07:06:51 +0600

    RatingControlを追加
    なぜかdefaultなんちゃら=Releaseが追加された…

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Toru Matsuoka on 2017/06/27.
+//  Copyright © 2017年 Toru Matsuoka. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+}
