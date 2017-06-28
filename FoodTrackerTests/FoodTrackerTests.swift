6fc967c82156d739b987e52d5d97d3a0591ca1fb
lettas <lettas0726@gmail.com>
2017-06-28 07:46:34 +0600

    高すぎるratingは失敗してほしい人生だった

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -28,6 +28,10 @@ class FoodTrackerTests: XCTestCase {
         let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
         XCTAssertNil(negativeRatingMeal)
 
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+
         // Empty String
         let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
         XCTAssertNil(emptyStringMeal)
