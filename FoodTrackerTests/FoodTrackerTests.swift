7305712364ab9d673753341d454d375ac8f0d4d4
lettas <lettas0726@gmail.com>
2017-06-28 07:40:25 +0600

    Mealの初期化成功と失敗のテストを追加

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,4 +11,25 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
 
+    // MARK: Meal Class Tests
+
+    func testMealInitializationSucceeds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+
+        // Highest Positive Rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+
+    func testMealInitializationFails() {
+        // Negative Rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
 }
