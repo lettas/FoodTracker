47f7fa1d0fd7deb59d7f142c37d7eae98d387cb7
lettas <lettas0726@gmail.com>
2017-06-28 07:31:42 +0600

    Mealモデルを実装

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2017年 Toru Matsuoka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+
+    // MARK: Properties
+
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+
+    // MARK: Initialization
+
+    init?(name: String, photo: UIImage?, rating: Int) {
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
