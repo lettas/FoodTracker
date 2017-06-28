afd55f32044aeb74f85fb3a0f7680c6c283d5712
lettas <lettas0726@gmail.com>
2017-06-28 07:48:48 +0600

    ratingの上限を設けさせていただきます

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -19,9 +19,14 @@ class Meal {
     // MARK: Initialization
 
     init?(name: String, photo: UIImage?, rating: Int) {
-        if name.isEmpty || rating < 0 {
+        guard !name.isEmpty else {
             return nil
         }
+
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+
         self.name = name
         self.photo = photo
         self.rating = rating
