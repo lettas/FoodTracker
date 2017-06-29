aa61a353aacd2498cc17f0a665cb12736c87817c
lettas <lettas0726@gmail.com>
2017-06-29 07:21:31 +0600

    いまいちふわっとしてるprepareメソッドを実装

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import os.log
+
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -54,6 +56,25 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         photoImageView.image = selectedImage
         dismiss(animated: true, completion: nil)
     }
+
+    // MARK: Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        // saveButtonからprepareされた時だけ進めます（それ以外はきっとcancelButton）
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+
 
     // MARK: Actions
 
