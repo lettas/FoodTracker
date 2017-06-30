02052e7e616d521bf71c3b6d7206188e0a1fc62b
lettas <lettas0726@gmail.com>
2017-06-30 07:24:13 +0600

    updateとinsertで処理を分けるのさ
    tableView.indexPathForSelectedRowが更新されるタイミングが謎い

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -55,9 +55,17 @@ class MealTableViewController: UITableViewController {
 
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
-            let newIndexPath  = IndexPath(row: meals.count, section: 0)
-            meals.append(meal)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // update
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                // insert
+                let newIndexPath  = IndexPath(row: meals.count, section: 0)
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
         }
     }
 
